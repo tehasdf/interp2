81e6a30237dd6d10532893fa3236f058e858d80f
asdf <tehasdf@gmail.com>
2016-02-18 12:53:54 +0100

    add a test for the rules caching

--- interp2/test/test_interp.py
+++ interp2/test/test_interp.py
@@ -1,1 +1,36 @@
+from interp2.compiler import Compiler
 from interp2.interp import Interp
+
+netstringsGrammar = """
+nonzeroDigit = digit:x ?(x != '0')
+digits = <'0' | nonzeroDigit digit*>:i -> int(i)
+
+netstring = digits:length ':' <anything{length}>:string ',' -> string
+receiveNetstring = netstring:string -> receiver.netstringReceived(string)
+"""
+
+
+class NetstringsReceiver(object):
+    def __init__(self):
+        self.strings = []
+
+    def netstringReceived(self, string):
+        self.strings.append(string)
+
+
+class TestInterp(object):
+    def test_reuseParseTree(self):
+        receiver = NetstringsReceiver()
+
+        compiler = Compiler(netstringsGrammar)
+        rule = compiler.getRule('receiveNetstring')
+        parseTree = compiler.compileRule(rule)
+
+        interp = Interp(parseTree, names={'receiver': receiver})
+        interp.receive('3:aaa,')
+        assert receiver.strings == ['aaa']
+        parseTree = compiler.compileRule(rule)
+        interp = Interp(parseTree, names={'receiver': receiver})
+        interp.receive('2:bb,')
+
+        assert receiver.strings == ['aaa', 'bb']
