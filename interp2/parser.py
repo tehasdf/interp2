8b8a365301e90df4b0b026089bdb520762505b53
asdf <tehasdf@gmail.com>
2016-02-18 12:53:47 +0100

    parser: cache the compiled rules

--- interp2/parser.py
+++ interp2/parser.py
@@ -9,12 +9,20 @@ class ParsleyCompatibleParser(object):
         self._bindings = bindings
         self._bindings['receiver'] = receiver
 
+        self.compiler = Compiler(self._grammar)
+        self._rules = {}
         self._setup()
 
+
+    def getParseTree(self, name):
+        # XXX this must also cache .compileRule, not just .getRule
+        # but apparently some state is still held inside the parse tree?
+        if name not in self._rules:
+            self._rules[name] = self.compiler.getRule(name)
+        return self.compiler.compileRule(self._rules[name])
+
     def _setup(self):
-        compiler = Compiler(self._grammar)
-        rule = compiler.getRule(self._receiver.currentRule)
-        parseTree = compiler.compileRule(rule)
+        parseTree = self.getParseTree(self._receiver.currentRule)
         self._interp = Interp(parseTree, self._setup, names=self._bindings)
 
     def receive(self, data):
